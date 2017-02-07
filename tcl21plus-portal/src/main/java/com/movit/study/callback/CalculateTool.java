5ab31c466423726d6e83c3604e1e10a156aff82d
jerry.zheng <zhengrui_java@163.com>
2017-02-07 17:13:03 +0800

    回调函数

---
+++ tcl21plus-portal/src/main/java/com/movit/study/callback/CalculateTool.java
@@ -0,0 +1,11 @@
+package com.movit.study.callback;
+
+/**
+ * Created by Administrator on 2017/2/7.
+ */
+public class CalculateTool {
+    public static void caculate(int a, int b, XiaoMing xiaoMing) {
+        int sum = a + b;
+        xiaoMing.caculate(a, b, sum);
+    }
+}
