5561baa6e59f2d7b84993d9f7c870b0e46e9852a
Jerry.Zheng <zhengrui_java@163.com>
2017-04-11 16:14:53 +0800

    luan

---
+++ tcl21plus-portal/src/main/java/com/movit/study/spring/bean_factory_aware/UserBeanFactoryAware.java
@@ -0,0 +1,21 @@
+package com.movit.study.spring.bean_factory_aware;
+
+import com.movit.study.model.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class UserBeanFactoryAware implements BeanFactoryAware {
+    private BeanFactory beanFactory;
+
+    public User getUser(){
+        return beanFactory.getBean(User.class);
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+}
---
+++ tcl21plus-portal/src/main/java/com/movit/study/spring/bean_factory_aware/UserBeanFactoryAwareTest.java
@@ -0,0 +1,20 @@
+package com.movit.study.spring.bean_factory_aware;
+
+import com.movit.study.spring.no_static_method_factory.DruidConnectionNew;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class UserBeanFactoryAwareTest {
+    public static void main(String[]args){
+        XmlBeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("E:\\git_down\\tcl21plus\\tcl21plus-portal\\src\\main\\resources\\study\\spring\\bean_factory_aware.xml"));
+        UserBeanFactoryAware userBeanFactoryAware = beanFactory.getBean(UserBeanFactoryAware.class);
+        userBeanFactoryAware.setBeanFactory(beanFactory);
+
+        for (int i=0; i<3; i++) {
+            System.out.println(userBeanFactoryAware.getUser());
+        }
+    }
+}
