fc7287ff769e4f2284e1625b528fe4db467d637a
Jerry.Zheng <zhengrui_java@163.com>
2017-04-11 15:55:58 +0800

    luan

--- tcl21plus-portal/src/main/java/com/movit/study/spring/factory_bean/GetUserBean.java
+++ tcl21plus-portal/src/main/java/com/movit/study/spring/factory_bean/GetUserBean.java
@@ -1,11 +1,14 @@
 package com.movit.study.spring.factory_bean;
 
 import com.movit.study.model.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 
 /**
  * Created by Administrator on 2017/4/11.
  */
-public class GetUserBean {
+public class GetUserBean{
     private User user;
 
     public User getUser() {
--- tcl21plus-portal/src/main/java/com/movit/study/spring/factory_bean/UserFactoryBeanTest.java
+++ tcl21plus-portal/src/main/java/com/movit/study/spring/factory_bean/UserFactoryBeanTest.java
@@ -14,7 +14,10 @@ public class UserFactoryBeanTest {
 
         //UserFactoryBean虽然是prototype的,但以下连续获取三个对象的实例却还是一样的
         //该问题是由于第一个实例注入GetUserBean后,GetUserBean再也没有重新向容器申请新的实例
-        //在UserFactoryBeanTest4Prototype中给出解决方案
+        //在UserFactoryBeanTest4Prototype中给出
+        //解决方案1 通过 <lookup-method name="getUser" bean="userFactoryBean"/>注入依赖
+        //解决方案2 通过实现BeanFactoryAware,让GetUserBean拥有BeanFactory引用,
+        // 这样每次获取UserFactoryBean时,可以通过BeanFactory的getBean()方法
         for (int i=0; i<3; i++) {
             GetUserBean getUserBean = beanFactory.getBean(GetUserBean.class);
             System.out.println(getUserBean.getUser());
