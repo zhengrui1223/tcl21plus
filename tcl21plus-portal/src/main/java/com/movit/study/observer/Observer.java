cc2c3cb7fb27097ac298733ebb81c28bd5bd14c8
jerry.zheng <zhengrui_java@163.com>
2017-04-12 12:39:05 +0800

    观察者模式

---
+++ tcl21plus-portal/src/main/java/com/movit/study/observer/Observer.java
@@ -0,0 +1,9 @@
+package com.movit.study.observer;
+
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+public abstract class Observer {
+
+    public abstract void update(Subject subject);
+}
---
+++ tcl21plus-portal/src/main/java/com/movit/study/observer/ObserverTest.java
@@ -0,0 +1,14 @@
+package com.movit.study.observer;
+
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+public class ObserverTest {
+    public static void main(String []args){
+        ConcreteSubject subject = new ConcreteSubject();
+        Observer observer = new ConcreteObserver();
+
+        subject.attach(observer);
+        subject.change("11111111111");
+    }
+}
