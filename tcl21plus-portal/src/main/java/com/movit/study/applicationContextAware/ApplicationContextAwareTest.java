138818950c3b5bcc9ef53c42ad0bf175a31dd431
zhengrui1223 <zhengrui_java@163.com>
2017-08-08 09:05:25 +0800

    ResourceLoaderAware及ApplicationContextAware

---
+++ tcl21plus-portal/src/main/java/com/movit/study/applicationContextAware/ApplicationContextAwareTest.java
@@ -0,0 +1,33 @@
+package com.movit.study.applicationContextAware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+/**
+ * 容器启动的时候，就会自动将当前ApplicationContext容器本身
+    注入到ApplicationContextAwareTest中，因为ApplicationContext类型容器可以自动识别Aware接口
+    同样的ResourceLoaderAware也是如此
+ */
+@Component
+public class ApplicationContextAwareTest implements ApplicationContextAware {
+    private ResourceLoader resourceLoader;
+
+    public ApplicationContextAwareTest() {
+    }
+
+    public Resource getResourceByPath(String path) {
+        return getResourceLoader().getResource(path);
+    }
+
+    public ResourceLoader getResourceLoader() {
+        return resourceLoader;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.resourceLoader = applicationContext;
+    }
+}
---
+++ tcl21plus-portal/src/main/java/com/movit/study/controller/ApplicationContextAwareTestController.java
@@ -0,0 +1,34 @@
+package com.movit.study.controller;
+
+import com.movit.study.applicationContextAware.ApplicationContextAwareTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by MOVITECH-5-e450 on 2017/8/8.
+ */
+
+@RequestMapping("/applicationContextAwareTest")
+@Controller
+public class ApplicationContextAwareTestController {
+
+    @Autowired
+    private ApplicationContextAwareTest applicationContextAwareTest;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void test(HttpServletResponse response) {
+        Resource resource = applicationContextAwareTest.getResourceByPath("spring/spring-dao.xml");
+        try {
+            response.getWriter().write(resource.getFile().getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
