4ec22efb13f67a27b97a1aeb230eb68dbc32dc2a
zhengrui1223 <zhengrui_java@163.com>
2018-04-15 22:43:10 +0800

    lambda study

---
+++ tcl21plus-portal/src/main/java/com/movit/study/java8/lambda/ListTest.java
@@ -0,0 +1,52 @@
+package com.movit.study.java8.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/************************************************************
+ * @Description: TODO
+ * @Author: jerry.zheng
+ * @Date 2018-04-15 16:27
+ ************************************************************/
+
+public class ListTest {
+    public static void main(String[] args) {
+        // Java 8之前：
+        List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        for (String feature : features) {
+            //System.out.println(feature);
+        }
+
+        List<String> features2 = new ArrayList<>();
+
+        //遍历方式1，有局部变量型
+        //features.forEach(item -> System.out.println(item));
+
+        //遍历方式2，无局部变量型
+        //features.forEach(System.out::println);
+
+        //遍历集合并复制
+        //features.forEach(item -> features2.add(item));
+
+        //复合操作，遍历打印并复制
+        /*features.forEach(item -> {
+            System.out.println(item);
+            features2.add(item + "2");
+
+            features2.forEach(item2 -> System.out.println(item2));
+        });*/
+
+        //过滤以L开头的字符串
+        //filter(features, (str) -> str.startsWith("L"));
+    }
+
+    public static void filter(List<String> names, Predicate condition) {
+        for(String name: names)  {
+            if(condition.test(name)) {
+                System.out.println(name + " ");
+            }
+        }
+    }
+}
---
+++ tcl21plus-portal/src/main/java/com/movit/study/java8/lambda/RunnableTest.java
@@ -0,0 +1,21 @@
+package com.movit.study.java8.lambda;
+
+/************************************************************
+ * @Description: TODO
+ * @Author: jerry.zheng
+ * @Date 2018-04-15 16:14
+ ************************************************************/
+
+public class RunnableTest {
+    public static void main(String[] args) {
+
+        new Thread(new Runnable() {
+            public void run() {
+                System.out.println("before java8 create a new thread.");
+            }
+        });
+
+        new Thread(() -> System.out.println("1111111")).start();
+
+    }
+}
